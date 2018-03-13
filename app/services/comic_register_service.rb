77142d93c5104383c575dcfeea7adc292635a2ad
ttuan <tuantv.nhnd@gmail.com>
2018-03-13 18:21:29 +0700

    Register for multiple comics at same time

--- app/services/comic_register_service.rb
+++ app/services/comic_register_service.rb
@@ -1,35 +1,38 @@
 class ComicRegisterService
-  attr_reader :comic_name, :current_user
+  attr_reader :comic_names, :current_user
 
-  def initialize comic_name, current_user
-    @comic_name = comic_name
+  def initialize comic_names, current_user
+    @comic_names = comic_names
     @current_user = current_user
   end
 
   def perform
-    if comic_info
-      comic = Comic.find_or_create_by comic_info
-      current_user.user_comics.find_or_create_by comic_id: comic.id
-      return
-    else
-      Dialogflow::PostMan.new("register_fail #{comic_name}")
-        .perform[:result][:fulfillment][:speech]
+    comic_names.each do |comic_name|
+      info = comic_info comic_name
+      if info
+        comic = Comic.find_or_create_by info
+        current_user.user_comics.find_or_create_by comic_id: comic.id
+      else
+        return Dialogflow::PostMan.new("register_fail #{comic_name}")
+          .perform[:result][:fulfillment][:speech]
+      end
     end
+    return
   end
 
   private
-  def comic_info
+  def comic_info comic_name
     agent = Mechanize.new
     sanitized_name = comic_name.sub(' ', '+')
     page = agent.get "#{Settings.comic.base_url}?s=#{sanitized_name}&q=#{sanitized_name}"
     if page.search(".update_item").present?
       comic = page.search(".update_item").first.search("a").first
       name, url = comic.attr("title"), comic.attr("href")
-      {name: name, url: url} if nice_result?(name)
+      {name: name, url: url} if nice_result?(name, comic_name)
     end
   end
 
-  def nice_result? name
+  def nice_result? name, comic_name
     name.downcase.include?(comic_name.downcase)
   end
 end
