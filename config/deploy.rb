ff939da6a860e7056140310f2b944f85856187d4
ttuan <tuantv.nhnd@gmail.com>
2018-04-05 15:28:36 +0700

    Config for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,47 +1,24 @@
 require 'mina/rails'
+require 'mina/bundler'
+require 'mina/puma'
 require 'mina/git'
-# require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
-require 'mina/rvm'    # for rvm support. (https://rvm.io)
-
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
+require 'mina/rvm'
 
 set :application_name, ENV["APPLICATION_NAME"]
-set :domain, ENV["DOMAIN"]
 set :user, "deploy"
+set :domain, ENV["DOMAIN"]
 set :deploy_to, ENV["DEPLOY_PATH"]
 set :repository, ENV["GIT_REPO"]
 set :branch, ENV["BRANCH"]
+set :rails_env, ENV["RAILS_ENV"]
+set :shared_dirs, fetch(:shared_dirs, []).push('log', 'tmp/pids', 'tmp/sockets', 'public/uploads')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb')
 
-# Optional settings:
-#   set :user, 'foobar'          # Username in the server to SSH to.
-#   set :port, '30000'           # SSH port number.
-#   set :forward_agent, true     # SSH forward_agent.
-
-# Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
-# Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
-# run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
-# set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
-# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# This task is the environment that is loaded for all remote run commands, such as
-# `mina deploy` or `mina rake`.
 task :remote_environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
-  # invoke :'rbenv:load'
-
-  # For those using RVM, use this to load an RVM version@gemset.
   invoke :'rvm:use', 'ruby-2.5.0'
 end
 
-# Put any custom commands you need to run at setup
-# All paths in `shared_dirs` and `shared_paths` will be created on their own.
 task :setup do
-  # command %{rbenv install 2.3.0 --skip-existing}
   command %[touch "#{fetch(:shared_path)}/config/database.yml"]
   command %[touch "#{fetch(:shared_path)}/config/secrets.yml"]
   command %[touch "#{fetch(:shared_path)}/config/puma.rb"]
@@ -50,12 +27,8 @@ end
 
 desc "Deploys the current version to the server."
 task :deploy do
-  # uncomment this line to make sure you pushed your local branch to the remote origin
-  # invoke :'git:ensure_pushed'
   deploy do
     comment "Deploying #{fetch(:application_name)} to #{fetch(:domain)}:#{fetch(:deploy_to)}"
-    # Put things that will set up an empty directory into a fully set-up
-    # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
@@ -64,18 +37,7 @@ task :deploy do
     invoke :'deploy:cleanup'
 
     on :launch do
-      in_path(fetch(:current_path)) do
-        command %{mkdir -p tmp/}
-        command %{touch tmp/restart.txt}
-      end
       invoke :'puma:phased_restart'
     end
   end
-
-  # you can use `run :local` to run tasks on local machine before of after the deploy scripts
-  # run(:local){ say 'done' }
 end
-
-# For help in making your deploy script, see the Mina documentation:
-#
-#  - https://github.com/mina-deploy/mina/tree/master/docs
--- config/puma.rb
+++ config/puma.rb
@@ -2,16 +2,16 @@
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
-bind  "unix:///home/deploy/deployed_folder/shared/tmp/sockets/puma.sock"
-pidfile "/home/deploy/deployed_folder/shared/tmp/pids/puma.pid"
-state_path "/home/deploy/deployed_folder/shared/tmp/sockets/puma.state"
-directory "/home/deploy/deployed_folder/current"
+bind  "unix:///home/deploy/shared/tmp/sockets/puma.sock"
+pidfile "/home/deploy/shared/tmp/pids/puma.pid"
+state_path "/home/deploy/shared/tmp/sockets/puma.state"
+directory "/home/deploy/current"
 
 workers 2
 threads 1,2
 
 daemonize true
 
-activate_control_app 'unix:///home/deploy/deployed_folder/shared/tmp/sockets/pumactl.sock'
+activate_control_app 'unix:///home/deploy/shared/tmp/sockets/pumactl.sock'
 
 prune_bundler
